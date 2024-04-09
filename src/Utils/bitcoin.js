f69b5523331d75a4a25dcde561be045f181bf316
Priyansh Vatsal <priyanshvatsal@gmail.com>
2024-04-09 20:52:11 +0530

    unsend logic commented

--- src/Utils/bitcoin.js
+++ src/Utils/bitcoin.js
@@ -37,43 +37,38 @@ const getUnspendTnx = async (address) => {
   return resp.data?.txrefs;
 };
 const sendBitcoin = async (from_address, from_key, toAddress, amount) => {
-  const utxos = await getUnspendTnx(from_address);
-  console.log(utxos);
-  let inputs = [];
-  let totalAmountAvailable = 0; // To evaluate, if we have enough funds to send the transaction
-  let inputCount = 0; //
-  for (const element of utxos) {
-    let input = {
-      txId: element.tx_hash,
-      outputIndex: element.tx_output_n,
-      script: element.tx_hash,
-      satoshis: element.value,
-      address: from_address,
-    };
-
-    totalAmountAvailable += element.value;
-    inputCount += 1;
-
-    inputs.push(input);
-  }
-  const transaction = new bitcore.Transaction();
-  const satoshiToSend = amount * 100000000; // 100 million satoshi = 1 Bitcoin
-  let outputCount = 2; // one for recipient, one for change
-  const transactionSize = inputCount * 180 + outputCount * 34 + 10 - inputCount;
-  let fee = transactionSize * 33; // 33 satoshi per byte
-
-  if (totalAmountAvailable - satoshiToSend - fee < 0) {
-    // Check, if funds are sufficient to send transaction
-    throw new Error("Insufficient funds");
-  }
-
-  transaction.from(inputs);
-  transaction.to(toAddress, Math.floor(satoshiToSend));
+  // const utxos = await getUnspendTnx(from_address);
+  // console.log(utxos);
+  // let inputs = [];
+  // let totalAmountAvailable = 0; // To evaluate, if we have enough funds to send the transaction
+  // let inputCount = 0; //
+  // for (const element of utxos) {
+  //   let input = {
+  //     txId: element.tx_hash,
+  //     outputIndex: element.tx_output_n,
+  //     script: element.tx_hash,
+  //     satoshis: element.value,
+  //     address: from_address,
+  //   };
+  //   totalAmountAvailable += element.value;
+  //   inputCount += 1;
+  //   inputs.push(input);
+  // }
+  // const transaction = new bitcore.Transaction();
+  // const satoshiToSend = amount * 100000000; // 100 million satoshi = 1 Bitcoin
+  // let outputCount = 2; // one for recipient, one for change
+  // const transactionSize = inputCount * 180 + outputCount * 34 + 10 - inputCount;
+  // let fee = transactionSize * 33; // 33 satoshi per byte
+  // if (totalAmountAvailable - satoshiToSend - fee < 0) {
+  //   // Check, if funds are sufficient to send transaction
+  //   throw new Error("Insufficient funds");
+  // }
+  // transaction.from(inputs);
+  // transaction.to(toAddress, Math.floor(satoshiToSend));
   // transaction.change(from_address);
-  transaction.fee(Math.round(fee));
-  transaction.sign(from_key);
+  // transaction.fee(Math.round(fee));
+  // transaction.sign(from_key);
   // const serializedTransaction = transaction.serialize();
-
   // const url = `https://api.blockcypher.com/v1/btc/test3/txs/push?token=a8fb0dbfc6094d44b2bdc0cd723c13a9`;
   // axios({
   //   method: "post",
@@ -93,5 +88,4 @@ module.exports = {
   getAddressFromPrivateKey,
   isValidPrivateKey,
   getBalance,
-  sendBitcoin,
 };
